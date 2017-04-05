cde3de66f4a2aea547e2934a820fd9b2f194b7ea
Rowanne Willett <rowannewillett@gmail.com>
2017-04-05 21:17:42 +0100

    updated add_post.php and added homepage.php

--- add_post.php
+++ add_post.php
@@ -4,7 +4,7 @@ if(isset($_POST['contents'], $_POST['category'])){
     //getting rid of whitespace
     $title      = trim($_POST['title']);
     $contents   = trim($_POST['contents']);
-    //something new
+   
     if (empty ($title)){
         
     }
@@ -35,17 +35,30 @@ if(isset($_POST['contents'], $_POST['category'])){
             
             <div>
                 <label for="title">Title</label>
-                <input type="text" name="title">
+                <input type="text" name="title" value="<?php if ( isset($_POST['title']) ) echo $_POST['title']; ?>">
             </div>
             
             <div>
                 <label for="contents"> Contents </label>
-                <textarea name="contents" rows="15" cols="50"></textarea>
+                <textarea name="contents" rows="15" cols="50"><?php if ( isset($_POST['contents']) ) echo $_POST['contents']; ?></textarea>
             </div>
             
             <div>
-                <label for="category"> Category </label>
-                <select name="category"></select>
+                <label for="musicplayer"> Music Player </label>
+                <select name="musicplayer"></select>
+            </div>
+            
+            <div>
+                <label for="tags"> Tags </label>
+                <select name="tags"></select>
+                <?php
+                foreach ( get_tags() as $tags ) {
+                    ?>
+                <option value ="<?php echo $tags['id']; ?>"><?php echo $tags['name']; ?></option>
+                <?php
+                }
+                ?>
+                
             </div>
             
             <div>
