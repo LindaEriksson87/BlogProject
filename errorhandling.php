83ea186d47ea18c3ce02573e3fb44e7a773770f4
Rowanne Willett <rowannewillett@gmail.com>
2017-04-05 20:26:49 +0100

    added errorhandling.php and add_post.php

---
+++ errorhandling.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(E_ALL);
+
+
+//limit for word count
+if ($wordcountofblog>500) {
+    trigger_error("Your blog post is too long - trim and try again! Max. 500 words.");
+}
+
+//incorrect username or password
+if ()
+    
+//uploading wrong file type, blog picture
+    // if !jpeg etc.
+
+// 
