f3899b945af3a77ba1d1d6baa7805f7d7e33a6da
Aisling <aislingporceddu@hotmail.co.uk>
2017-04-10 22:37:04 +0100

    Merging my changes to index

--- View/index.php
+++ View/index.php
@@ -12,6 +12,7 @@ require_once '../model/connection.php';
 <html>
 <head><title>Blog Homepage</title></head>
 
+
 <!--Including Bootstrap CSS -->
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
