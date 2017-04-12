30ff75e2d74e41b741db10ffe4fcb95619880d46
iberischris <kristina3333@hotmail.co.uk>
2017-04-12 13:02:39 +0100

    tag and query update on the database

--- Model/postClass.php
+++ Model/postClass.php
@@ -25,7 +25,7 @@ class POST
            $stmt->bindparam(":title", $title);
            $stmt->bindparam(":content", $content);
            $stmt->bindparam(":user_id", $_SESSION['user_session']);
-           //$stmt->bindparam(":tags", $tags);         
+           //$stmt->bindparam(":tags", $tags);   //TODO: Use tag ID from the tags table      
            $stmt->execute();
            
            return $stmt; 
@@ -61,6 +61,24 @@ class POST
         {
             $stmt = $this->db->prepare("SELECT * FROM posts WHERE user_id=:user_id");
             $stmt->bindparam(":user_id", $user_id);
+            $stmt->execute();
+            
+            return $stmt;
+        }
+        catch(PDOException $e)
+       {
+            echo $e->getMessage();
+       }    
+    }
+    
+    //This is a method to read all posts in the database written by a user searched for by tag. 
+    public function readTag($tag_id)
+    {
+        try
+        {
+            $stmt = $this->db->prepare("SELECT post_id, post_title, post_content, date, posts.tags, user_id , tags.tag_name
+                                        FROM posts INNER JOIN tags ON tags.tag_id =  posts.tags;  WHERE tags.tag_id = ':tag_id'");
+            $stmt->bindparam(":tag_id", $tag_id);
             $stmt->execute();
             
             return $stmt;
