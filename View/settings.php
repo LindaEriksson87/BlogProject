40a8372fd88cc2709ee53b9300798e876de815db
Rowanne Willett <rowannewillett@gmail.com>
2017-04-14 17:39:36 +0100

    adding updates settings.php page
    neWillett MINGW64 /c/xampp/htdocs/BlogProject (master)
    $ git add settings.

--- View/settings.php
+++ View/settings.php
@@ -31,10 +31,32 @@ else
     
     <div>
         <h3>Upload image</h3>
+        <!--RW - for posting a profile picture -->
+          <div id="post" class="collapse">
+            <form class ="col-sm-8 well well-lg" action="" method="post">
+                <div class="form-group">
+                <label for="image">Image</label>
+                <input type="image" name="title" value="<?php if ( isset($_POST['image']) ) echo $_POST['image']; ?>">                
+                </div>
+          </div>
         
         <h3>Update name</h3>
-        <h3>Update email</h3>
+               <div class="form-group">
+                <label for="name">Name</label>
+                <input class="form-control" type="text" name="name" value="<?php if ( isset($_POST['name']) ) echo $_POST['name']; ?>">
+               </div>
+        
+        <h3>Update Email</h3>
+               <div class="form-group">
+                <label for="email">Email</label>
+                <input class="form-control" type="text" name="email" value="<?php if ( isset($_POST['email']) ) echo $_POST['email']; ?>">
+               </div>
+        
         <h3>Add Biography</h3>
+            <div class="form-group">
+                <label for="bioraphy">Name</label>
+                <input class="form-control" type="text" name="biography" value="<?php if ( isset($_POST['biography']) ) echo $_POST['biography']; ?>">
+            </div>
     </div>  
     
   
