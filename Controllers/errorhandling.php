b6bba9e905adcb8a2bb659f1688c864dcd394cb0
iberischris <kristina3333@hotmail.co.uk>
2017-04-14 18:44:40 +0100

    error handling

--- Controllers/errorhandling.php
+++ Controllers/errorhandling.php
@@ -10,8 +10,49 @@ if ($wordcountofblog>500) {
 
 //incorrect username or password
 //if ()
-    
+  
+$passwordcharacters= "$2y$10$2aNyIXKZJSgjvN3GEEc6TO0AnweslZsWH3ygnZp9V6SV1z5pIaPve, NULL";
+if ($passwordchracters<=5) {
+  trigger_error("Your password must include a minimum of 5 characters");
+}
+
+ 
 //uploading wrong file type, blog picture
     // if !jpeg etc.
 
-// 
+
+
+
+
+
+
+
+// inapproatate text in submitted in the comment section 
+ $user='linda';// replace with actual logged in user
+        
+        $comment = trim($_POST['comment']);
+	
+	$error = false;
+	
+	if(empty($comment))
+	{
+		$comment_error='comment section is empty. Please enter your comment.';//comment validation
+		$error=true;
+	}
+        if(false === $error)
+	{
+      $cleanedComment = mysqli_real_escape_string($pdo, $comment);//removes any unwanted code submitted by the user before saving to database
+$sql = "INSERT INTO Comment (user, comment)
+VALUES ('$user','$cleanedComment')"; //include username and comments 
+      
+        }
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();	
+		
+	
+        
