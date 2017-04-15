10bd76bd39ce61ad9fd57f43b4bfd17eb6062f65
iberischris <kristina3333@hotmail.co.uk>
2017-04-15 23:11:42 +0100

    javascript
    adding javascript and making some changes to existing code

--- View/search.php
+++ View/search.php
@@ -87,7 +87,31 @@ else
             ?>
     </div>
         <div class="container col-sm-2"></div>
+        
+        <script>
+        <!-- Button trigger modal -->
+ <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myblog" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="Linda" id="Lindablog"></h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</script>
 </body>
 </html>
-
-
