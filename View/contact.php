7f40cd2acc698aaf0ce344a7d3bd85fb0cc9bbac
Linda Eriksson <linda.nibelheim@gmail.com>
2017-04-17 18:23:09 +0100

    Bug fixes

--- View/contact.php
+++ View/contact.php
@@ -103,7 +103,7 @@ Thank you for contacting Get Into Techno.
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"  crossorigin="anonymous">
 <link rel="stylesheet" href="CSS/additionalCSS.css">
-<link href="https://fonts.googleapis.com/css?family=Geostar+Fill|Raleway|Roboto|Stalinist+One" rel="stylesheet"
+<link href="https://fonts.googleapis.com/css?family=Geostar+Fill|Raleway|Roboto|Stalinist+One" rel="stylesheet">
 </head>
 
 
--- View/settings.php
+++ View/settings.php
@@ -151,7 +151,7 @@ if (isset($_POST['btn-img'])){
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="CSS/additionalCSS.css">
-<link href="https://fonts.googleapis.com/css?family=Geostar+Fill|Raleway|Roboto|Stalinist+One" rel="stylesheet"
+<link href="https://fonts.googleapis.com/css?family=Geostar+Fill|Raleway|Roboto|Stalinist+One" rel="stylesheet">
 <body>
     
 <?php 
