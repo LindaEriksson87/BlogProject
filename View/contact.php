8f114f038b40ae9ebf130edc19455abe0849c88a
Leonie Johnson <lejohnson75@gmail.com>
2017-04-17 15:33:52 +0100

    contact form update

--- View/contact.php
+++ View/contact.php
@@ -7,40 +7,87 @@ use function controllers\display\display;
 
 require_once '../model/connection.php';
 
-/*if($user->is_loggedin()!="")
-{
-    $user->redirect('index.php');
-}
-
-if(isset($_POST['btn-send']))
-{
-   $filteredFullname = filter_input(INPUT_POST,'txt_fullname', FILTER_SANITIZE_SPECIAL_CHARS);
-   $fullname_name = trim($filteredFirstname);
-   
-   $filteredEmail = filter_input(INPUT_POST,'txt_email', FILTER_SANITIZE_EMAIL);
-   $email = trim($filteredEmail);
-   
-   $filteredMessage = filter_input(INPUT_POST,'txt_message', FILTER_SANITIZE_SPECIAL_CHARS);
-   $message = trim($filteredMessage);
-   
-   
-   if($fullname_name=="") {
-      $error[] = "Please provide a name."; 
-   }
-   else if($email=="") {
-      $error[] = "Please provide an email address."; 
-   }
-   else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $error[] = 'Please enter a valid email address.';
-   }
-   else if($message=="") {
-      $error[] = "Please enter a message.";
-   }
-  else {
-      echo "Message has been sent";
-  }*/// May need to add additional code
-
+if(isset($_POST['email'])) {
+ 
+    // email to send to & subject line
+    $email_to = "lejohnson75@gmail.com";
+    $email_subject = "A Message to Get Into Techno";
+ 
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+ 
+    // validation expected data exists
+    if(!isset($_POST['first_name']) ||
+        !isset($_POST['last_name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+     
+ 
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name']; 
+    $email_from = $_POST['email']; 
+    $message = $_POST['message']; 
+ 
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+  if(!preg_match($string_exp,$first_name)) {
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  }
+  
+  if(!preg_match($string_exp,$last_name)) {
+    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+  }
+ 
+ 
+  if(strlen($message) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+ 
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+     
+ 
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
 ?>
+ 
+<h1>Thank you for contacting Get Into Techno. We will be in touch with you very soon.</h1>
+ 
+
 
 <!doctype html>
 <html>
